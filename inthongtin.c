1623f4bfe7762579abc3f11c3bba689d07ae66be
hoaihan241 <nguyenhan24012007@gmail.com>
2026-04-29 02:11:36 +0700

    inthongtin

---
+++ inthongtin.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void inBang(FILE *out, GiaSu ds[], int n) {
+    // Header
+    fprintf(out, "+----+----------------------+----------+--------------+--------+--------------+------------+----------------------+--------+\n");
+    fprintf(out, "| ID | Ten                  | Mon      | Khu vuc      | Phi    | Trinh do     | Danh gia   | Email               | Diem   |\n");
+    fprintf(out, "+----+----------------------+----------+--------------+--------+--------------+------------+----------------------+--------+\n");
+
+    // Data
+    for (int i = 0; i < n; i++) {
+        fprintf(out, "| %-2d | %-20s | %-8s | %-12s | %-6d | %-12s | %-10.1f | %-20s | %-6d |\n",
+            ds[i].id,
+            ds[i].ten,
+            ds[i].mon,
+            ds[i].khuVuc,
+            ds[i].hocPhi,
+            ds[i].trinhDo,
+            ds[i].danhGia,
+            ds[i].email,
+            ds[i].diem);
+    }
+
+    // Footer
+    fprintf(out, "+----+----------------------+----------+--------------+--------+--------------+------------+----------------------+--------+\n");
+}
+void inThongTin(GiaSu ds[], int n) {
+    FILE *f = fopen("ketqua.txt", "w");
+    if (f == NULL) {
+        printf("Khong mo duoc file\n");
+        return;
+    }
+
+    // In ra màn hình
+    printf("\nDANH SACH GIA SU:\n");
+    inBang(stdout, ds, n);
+
+    // Ghi vào file
+    fprintf(f, "DANH SACH GIA SU:\n");
+    inBang(f, ds, n);
+
+    fclose(f);
+}
